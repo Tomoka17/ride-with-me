88db916aa790d5bc439362802b73b630570e1d71
Tomoka17 <tomotomato.617@gmail.com>
2022-04-15 09:12:31 +0900

    deviseストロングパラメータ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-end
+    private
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(
+        :sign_up, keys: [:nickname, :genre_id, :bike_name, :age_id, :gender_id, :prefecture_id, :personality_id])
+    end
+  end
