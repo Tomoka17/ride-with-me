c0b1aceafbef826e6ebb4c2475a545b2554b7491
Tomoka17 <tomotomato.617@gmail.com>
2022-04-18 14:17:55 +0900

    lastcommit

--- app/controllers/tourings_controller.rb
+++ app/controllers/tourings_controller.rb
@@ -7,11 +7,23 @@ class TouringsController < ApplicationController
   end
 
   def new
-
+    #@touring = Touring.new
   end
 
   def create
-
+    @touring = Touring.new(touring_params)
+      if @touring.save
+        redirect_to "index"
+      else
+        render new
+      end
   end
 
+  private
+
+  def touring_params
+    params.require(:touring).permit(
+      :image, :title, :weather_id, :mileage, :record
+    ).merge(user_id: current_user.id)
+  end
 end
--- app/models/touring.rb
+++ app/models/touring.rb
@@ -1,13 +1,13 @@
 class Touring < ApplicationRecord
   belongs_to :user
   # has_many :comments
-  # has_one_attached :image
+  has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :weather
 
   with_options presence: true do
-    #validatas :image
+    validates :image
     validates :title, length: {maximum: 40}
     validates :weather_id, numericality: { only_integer: true, message: "can't be blank" }
     validates :mileage, numericality: { only_integer: true }
